b28ba03def8dfe5f0e192c8703adc33afa3a676b
mlynarst <tomekcm4@gmail.com>
2020-06-21 16:37:41 +0200

    v1.0

---
+++ src/com/tomek/AddServlet.java
@@ -0,0 +1,40 @@
+package com.tomek;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/add")
+public class AddServlet extends HttpServlet {
+
+	
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		
+		int  i = Integer.parseInt(req.getParameter("num1"));
+		int j =  Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j ;
+		
+		PrintWriter out = res.getWriter();
+		out.println("<html><body bgcolor='cyan'>");
+		out.println(k);
+		out.print("</body></html>");
+		
+/*		Cookie cookie = new Cookie("k", k+"");
+		res.addCookie(cookie);
+		
+		res.sendRedirect("sq");
+	*/
+	}
+	
+	
+}
